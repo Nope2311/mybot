e36dd2127755d8f38ff976b0a01a7ef67a1d0c4c
Nope23 <honganhdaotat@gmail.com>
2026-07-14 08:33:01 +0700

    Update mod.py

--- cogs/mod.py
+++ cogs/mod.py
@@ -1,4 +1,5 @@
 import discord
+import config
 from discord.ext import commands
 from discord import app_commands
 
@@ -6,5 +7,11 @@ class ModCommands(commands.Cog):
     def __init__(self, bot : commands.Bot):
         self.bot = bot
 
+    @app_commands.default_permissions(administrator=True)
+    @app_commands.command()
+    async def backup(inter : discord.Interaction):
+        config.backup_active = True
     
+async def setup(bot : commands.Bot):
+    await bot.add_cog(ModCommands(bot))
         
