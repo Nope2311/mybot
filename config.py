0c91b5ce8bacbe1db807ec715c260e2f592d54bd
Nope23 <honganhdaotat@gmail.com>
2026-07-14 07:43:48 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
 import json
 
+STREAK3 = "<a:streak3:1525696855890198530>"
+STREAK10 = "<a:streak10:1525696853390135488>"
+STREAK30 = "<a:streak50:1525696852341559306>"
+STREAK100 = "<a:streak100:1525696854304493620>"
+STREAK150_plus = "<a:streak150:1525696851137921144>"
+
 ## default_data
 
 pf_default = {"KLT" : 0, "Streak":0, "last_time_streak": 0,"last_time_mess" : 0, "message_today":0,"today_mess_allow" : False}
@@ -48,9 +54,5 @@ COINS = "<a:KLT_coin:1525697095993004174>"
 VERIFY = "<:7verify7:1495226080695681164>"
 CROSS = "<:7cross7:1495226068028883046>"
 
-STREAK3 = "<a:streak3:1525696855890198530>"
-STREAK10 = "<a:streak10:1525696853390135488>"
-STREAK30 = "<a:streak50:1525696852341559306>"
-STREAK100 = "<a:streak100:1525696854304493620>"
-STREAK150_plus = "<a:streak150:1525696851137921144>"
+
 
