9dc9193633aa41604dc29d479780861450b2d3b8
Nope23 <honganhdaotat@gmail.com>
2026-07-14 09:43:03 +0700

    Update profile.py

--- cogs/profile.py
+++ cogs/profile.py
@@ -40,6 +40,9 @@ class Profile(commands.Cog):
 
         streak = data[user_id]["Streak"]
         klt_coins = data[user_id]["KLT"]
+        boDem = format_time(current - data[user_id]["last_time_streak"])
+
+        boDem_current = str(boDem) if (data[user_id]["last_time_streak"] != 0) or (data[user_id]["last_time_mess"] != 0) else "Chưa cập nhật"
 
         emoji_streak = config.streak_emoji_change(streak)
 
@@ -57,7 +60,7 @@ class Profile(commands.Cog):
         pf_embed.add_field(name=f"{emoji_check_message} Tin nhắn đã gửi : ",
                            value=f"`{mess_check_plus} / {config.MAX_MESSAGE} `",inline=True)
         pf_embed.add_field(name=f"⌚ Bộ đếm chuỗi trong ngày : ",
-                    value=f"`{format_time(current - data[user_id]["last_time_streak"])}`",inline=True)
+                    value=f"`{boDem_current}`",inline=True)
         
         pf_embed.timestamp = datetime.now(timezone.utc)
 
